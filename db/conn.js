9e8be52f147a1eee32c430f858e46b8d762e40b6
juliawlett <julialeticia100@gmail.com>
2024-05-23 00:04:00 -0400

    uptade

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,9 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { //alterar nome do banco
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: require('mysql2')  // Adicione isso para garantir que o sequelize use o mysql2
+
 })
 
 try{
